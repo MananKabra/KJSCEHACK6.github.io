d8e5866868e9e17aaf254d794905da49cf49d585
MananKabra <manankabra200318@gmail.com>
2022-04-09 13:10:34 +0530

    header

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 window.addEventListener("scroll",function(){
     var scrollValue = window.pageYOffset;
+    var headerImage = document.querySelector("#header .header-image");
     console.log(scrollValue);
+
+    headerImage.style.transform = "translateY(" + scrollValue*0.4 + "px)";
 })
