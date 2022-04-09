5487bf970e6418f5fe6cab86ee90f579378e8e5c
MananKabra <manankabra200318@gmail.com>
2022-04-09 14:41:38 +0530

    media queries

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ window.addEventListener("scroll",function(){
     var headerImage = document.querySelector("#header .header-image");
     console.log(scrollValue);
 
-    headerImage.style.transform = "translateY(" + scrollValue*0.2 + "px)";
+    headerImage.style.transform = "translateY(" + scrollValue*-0.2 + "px)";
 
-    if(scrollValue>=390){
+    if(scrollValue>=1280){
         document.querySelector(".left-card").classList.remove("animation-stopper");
         document.querySelector(".center-card").classList.remove("animation-stopper");
         document.querySelector(".right-card").classList.remove("animation-stopper");
     }
 
-    if(scrollValue<390){
+    if(scrollValue<1280){
         document.querySelector(".left-card").classList.add("animation-stopper");
         document.querySelector(".center-card").classList.add("animation-stopper");
         document.querySelector(".right-card").classList.add("animation-stopper");
